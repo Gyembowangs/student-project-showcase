af3dbdb136a164be0dc6b848b4d7def262c7d6be
Gyembowangs <07230028.sherubtse@rub.edu.bt>
2025-06-02 20:28:36 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,  // important for Neon
-    }
+      rejectUnauthorized: false, // for Neon PostgreSQL
+    },
   },
-  logging: false,
 });
 
+module.exports = sequelize;
